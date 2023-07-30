264982d1c7d1954de8af79546109c46c667f6340
mobcoder-umesh <umesh.mobcoder@gmail.com>
2023-07-30 12:34:23 +0530

    add firebase

--- app/src/main/java/com/app/wiki/MainActivity.kt
+++ app/src/main/java/com/app/wiki/MainActivity.kt
@@ -2,11 +2,15 @@ package com.app.wiki
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.app.wiki.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
